46be292d2fa7b6a527265228f7b892b9a4e937a5
Sadia <sadiajhn@gmail.com>
2016-02-05 10:02:45 +0600

    adding a file

---
+++ CascadingPractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CascadingPractice.MvcApplication" Language="C#" %>
---
+++ CascadingPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CascadingPractice.Startup))]
+namespace CascadingPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
